b0f8770a0d934c15c245381475b820e95f999977
Muhamad-Ahmed <166923382+Muhamad-Ahmed@users.noreply.github.com>
2024-07-08 18:33:00 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 #! /usr/bin/env node
-//SHABANG
-import inquirer from "inquirer";
-const answer = await inquirer.prompt([
+"use strict";
+//The above line defines this whenever this code will run it will run on node. 
+//SHABANG : the above line is called SHABANG . used to publish your ts file or ts project on npm which can be used by whole world then.
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+// "." represents your current directory .
+const inquirer_1 = __importDefault(require("inquirer"));
+const answer = await inquirer_1.default.prompt([
     { message: "Enter first number", name: "first_number", type: "number" },
     { message: "Enter second number", name: "second_number", type: "number" },
     {
